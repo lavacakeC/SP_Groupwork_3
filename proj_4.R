09b08b5d46187719848a54195a6e69e3fdd8f0c1
RuishuoXi <s2305931@ed.ac.uk>
2022-11-17 19:06:25 +0000

    changes

--- proj_4.R
+++ proj_4.R
@@ -1,92 +1,91 @@
 
 
-# Codes implement Newton's method for minimization of functions.
-# Newton's method uses the first few terms of the Taylor series 
-# for the function f(x) to iterate and seek the minimum of approximation 
-# at each step.
-# Step 1: Calculate the objective function, gradient function and 
-# Hessian matrix function
-# Step 2: Test whether convergence by seeing whether all elements of
-# the gradient vector have absolute value less than tol times the absolute
-# value of the objective function plus f-scale. And then test whether Hessian
-# is positive definite.
-# Step 3: If Hessian is not positive definite, perturb it by setting the 
-# multiplier to a small multiple of a matrix norm of the Hessian. If it still 
-# doesn't do it, repeatedly multiply the multiplier until we get the positive 
-# definite Hessian
-# Step 4: Solve for the search direction delta
-# Step 5: If the value of function on theta plus delta is not less than the 
-# value of function on theta, then we repeatedly halve delta until it is
-# Step 6: Iterate theta by theta plus delta, increment by k=1 and return to step 1
+## Codes implement Newton's method for minimization of functions.
+## Newton's method uses the first few terms of the Taylor series 
+## for the function f(x) to iterate and seek the minimum of approximation 
+## at each step.
+## Step 1: Calculate the objective function, gradient function and 
+## Hessian matrix function
+## Step 2: Test whether convergence by seeing whether all elements of
+## the gradient vector have absolute value less than tol times the absolute
+## value of the objective function plus f-scale. And then test whether Hessian
+## is positive definite.
+## Step 3: If Hessian is not positive definite, perturb it by setting the 
+## multiplier to a small multiple of a matrix norm of the Hessian. If it still 
+## doesn't do it, repeatedly multiply the multiplier until we get the positive 
+## definite Hessian
+## Step 4: Solve for the search direction delta
+## Step 5: If the value of function on theta plus delta is not less than the 
+## value of function on theta, then we repeatedly halve delta until it is
+## Step 6: Iterate theta by theta plus delta, increment by k=1 and return to step 1
 
 
 func_detail <- function(theta, func, grad, hess, eps, iter, ...) {
   
-  # This function is a started function that 
-  # get the result from objective function, 
-  # gradient function and hessian matrix function.
-  # The inputs is a vector of initial values theta 
-  # Function will return the result from objective 
-  # function, gradient vector of the objective 
-  # and hessian matrix of the objective.
+  ## This function is a started function that 
+  ## get the result from objective function, 
+  ## gradient function and hessian matrix function.
+  ## The inputs is a vector of initial values theta 
+  ## Function will return the result from objective 
+  ## function, gradient vector of the objective 
+  ## and hessian matrix of the objective.
   
-  func_result <- func(theta, ...) # get result from objective function 
+  func_result <- func(theta, ...) ## get result from objective function 
   
   if (iter == 0 & !is.finite(func_result)) stop("objective is not finite at the initial theta")
-  # use attr() to specify attributes "grad" and associate gradient function's results
+  ## use attr() to specify attributes "grad" and associate gradient function's results
   attr(func_result, "grad") <- grad(theta, ...)
   
   if (iter == 0 & !all(is.finite(attr(func_result, "grad")))) stop("derivatives are not finite at the initial theta")
   
   if (is.null(hess)) {
-    # if hessian matrix function not supplied, 
-    # set a new square matrix and calculate each value in the matrix
+    ## if hessian matrix function not supplied, 
+    ## set a new square matrix and calculate each value in the matrix
     hess <- matrix(0, length(theta), length(theta))
     
     for (i in 1:length(theta)){
-      ############################ need details #######################
       theta_1 <- theta
-      theta_1[i] <- theta_1[i] + eps
-      grad_theta_1 <- grad(theta_1, ...)
-      hess[i,] <- (grad_theta_1 - attr(func_result, "grad")) / eps
+      theta_1[i] <- theta_1[i] + eps ## increase th0[i] by eps
+      grad_theta_1 <- grad(theta_1, ...) 
+      hess[i,] <- (grad_theta_1 - attr(func_result, "grad")) / eps # approximate -dl/dth[i]
     }
   }
-  # If Hessian matrix function is provide use attr() to specify attributes and 
-  # associate hessian matrix function's results.
+  ## If Hessian matrix function is provide use attr() to specify attributes and 
+  ## associate hessian matrix function's results.
   else hess <- hess(theta, ...)
   attr(func_result, "hess") <- hess
   return(func_result)
-}
+} ## func_detail
 
 
 perturb_hess <- function(func_result) {
   
-  # This function calculate perturbed hessian matrix
-  # when the original hessian matrix is not positive definite
-  # The input is results from func_detail function
-  # It will return the same value except that the 
-  # hessian matrix becomes a perturbed hessian matrix
+  ## This function calculate perturbed hessian matrix
+  ## when the original hessian matrix is not positive definite
+  ## The input is results from func_detail function
+  ## It will return the same value except that the 
+  ## hessian matrix becomes a perturbed hessian matrix
   
   flag = FALSE
-  multiple <- 1e-6   # Finite difference intervals
-  hess <- attr(func_result, "hess")  # get original hessian matrix
+  multiple <- 1e-6   ## Finite difference intervals
+  hess <- attr(func_result, "hess")  ## get original hessian matrix
   while (!flag) {
-    # flag is false, multiply the identity matrix by 
-    # the original Hessian norm, and then multiply by finite difference intervals
+    ## flag is false, multiply the identity matrix by 
+    ## the original Hessian norm, and then multiply by finite difference intervals
     hess <- hess + multiple * norm(hess) * diag(dim(hess)[1])
     
-    flag <- TRUE  # change the flag
+    flag <- TRUE  ## change the flag
     
     tryCatch(
       {
-        # Try to inverse hessian matrix
+        ## Try to inverse hessian matrix
         attr(func_result, "hess_inverse") <- chol2inv(chol(hess))
       }, 
       error =  function(e){
-        flag <<- FALSE   # flag changed if there has error
+        flag <<- FALSE   ## flag changed if there has error
       })
     
-    multiple <- multiple * 10 # set new finite difference intervals value
+    multiple <- multiple * 10 ## set new finite difference intervals value
     
   }
   
@@ -95,45 +94,45 @@ perturb_hess <- function(func_result) {
 
 theta_calculate <- function(theta, func, func_result, max.half, ...) {
   
-  # This function is helper function to do calculate
-  # to get new delta with issue errors and warnings.
-  # The inputs are vector of initial values
-  # It will return new theta
+  ## This function is helper function to do calculate
+  ## to get new delta with issue errors and warnings.
+  ## The inputs are vector of initial values
+  ## It will return new theta
   
   half <- 0
   
-  # Multiply inverse of hessian matrix and gradient vector to get delta
+  ## Multiply inverse of hessian matrix and gradient vector to get delta
   delta <- - attr(func_result, "hess_inverse") %*% attr(func_result, "grad")
   
-  # Flag is true when new theta smaller than original result
+  ## Flag is true when new theta smaller than original result
   flag_delta <- func(theta + delta, ...) < func(theta, ...)
   
   delta_new <- delta
   
   if (!is.finite(func(theta + delta, ...))) {
+    ## when delta is greater than original theta,
+    delta_new <- delta / 2 ## set delta halve each time
+    half <- 1 ## set new half each time plus 1
     
-    delta_new <- delta / 2
-    half <- 1
-    
-    # Flag is true when new theta smaller than original result
+    ## Flag is true when new theta smaller than original result
     flag_delta <- func(theta + delta_new, ...) < func(theta, ...)
     
   }
   
   while (flag_delta == FALSE & half <= max.half) {
-    # when delta is greater than original theta,
-    half <- half + 1 # set new half each time plus 1
+    ## when delta is greater than original theta and half is smaller than or equal to max.half
+    half <- half + 1 ## set new half each time plus 1
     delta <- delta_new
-    delta_new <- delta_new / 2 # set delta halve each time
+    delta_new <- delta_new / 2 ## set delta halve each time
     
-    if (!is.finite(func(theta + delta, ...))) {
+    if (!is.finite(func(theta + delta, ...))) { #####
       delta_new <- (delta + delta_new) / 2
     }
     
     flag_delta <- func(theta + delta_new, ...) < func(theta, ...)
   }
   
-  if (flag_delta == TRUE) return(theta + delta_new) # return new theta ????
+  if (flag_delta == TRUE) return(theta + delta_new) ## return new theta
   
   if (half > max.half) {
     stop("The step has failed to improve the objective.")
@@ -144,15 +143,15 @@ theta_calculate <- function(theta, func, func_result, max.half, ...) {
 
 newt <- function(theta,func,grad,hess=NULL,...,tol=1e-8,fscale=1,maxit=100,max.half=20,eps=1e-6){
   
-  # This function is main optimization function 
+  ## This function is main optimization function 
   
-  iter <- 0 # number of iterations
+  iter <- 0 ## number of iterations
   for (i in 1:maxit) {
-    # func_result <- func_detail(theta, func, grad, hess, eps, ...)
-    # 
+    ## func_result <- func_detail(theta, func, grad, hess, eps, ...)
+    ## 
     func_result <- func_detail(theta = theta, func = func, grad = grad, hess = hess, eps = eps, iter, ...)
     
-    flag_grad = FALSE ### change
+    flag_grad = FALSE ## change
     flag_hess = TRUE
 
     if (all(abs(attr(func_result, "grad")) < tol * (abs(func_result) + fscale))) flag_grad = TRUE
@@ -160,7 +159,7 @@ newt <- function(theta,func,grad,hess=NULL,...,tol=1e-8,fscale=1,maxit=100,max.h
     
     tryCatch(
       {
-        attr(func_result, "hess_inverse") <- chol2inv(chol(attr(func_result, "hess")))  ### change
+        attr(func_result, "hess_inverse") <- chol2inv(chol(attr(func_result, "hess")))  ## change
       }, 
       error =  function(e){
         flag_hess <<- FALSE
